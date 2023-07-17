efc4c4b0d4a1d2222ab4136420518429f9923a57
Eddychege <eddychege423@gmail.com>
2023-07-17 15:33:19 +0300

    copy-cut-paste

--- 1-edges.py
+++ 1-edges.py
@@ -1,7 +1,8 @@
 word = "Holberton"
+word_first_3= word[:3]
+word_last_2= word[7:]
+middle_word= word[1:8]
 
-print(word[:3])
-
-print(word[7:])
-
-print(word[1:8])
+print(word_first_3)
+print(word_last_2)
+print(middle_word)
