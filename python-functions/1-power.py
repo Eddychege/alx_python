97eaeb78f9df9851bf6a7e39494180a4cb013dea
Eddychege <eddychege423@gmail.com>
2023-07-21 17:39:52 +0300

    changes

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,7 +1,19 @@
 def pow(a, b):
     return a**b
 print(pow(2, 2))
+
+def pow(a, b):
+    return a**b
 print(pow(98, 2))
+
+def pow(a, b):
+    return a**b
 print(pow(98, 0))
+
+def pow(a, b):
+    return a**b
 print(pow(100, -2))
+
+def pow(a, b):
+    return a**b
 print(pow(-4, 5))
