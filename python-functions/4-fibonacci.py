0e9b7cfaf309af97458511ce98629d255186df27
Eddychege <eddychege423@gmail.com>
2023-07-21 17:54:44 +0300

    Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fib_list = [0, 1]
+    while len(fib_list) < n:
+        next_fib = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_fib)
+
+    return fib_list
