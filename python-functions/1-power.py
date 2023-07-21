73f74edbbd9421d506bc3c0d6ae95c7811ec8116
Eddychege <eddychege423@gmail.com>
2023-07-21 17:57:49 +0300

    a^b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,19 +1,2 @@
 def pow(a, b):
     return a**b
-print(pow(2, 2))
-
-def pow(a, b):
-    return a**b
-print(pow(98, 2))
-
-def pow(a, b):
-    return a**b
-print(pow(98, 0))
-
-def pow(a, b):
-    return a**b
-print(pow(100, -2))
-
-def pow(a, b):
-    return a**b
-print(pow(-4, 5))
